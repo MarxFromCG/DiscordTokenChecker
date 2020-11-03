5a16cdd187fa465d47edf7ea77bbec2e2b483bc8
Marx <68349804+MarxFromCG@users.noreply.github.com>
2020-11-03 15:28:55 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+from colorama import Fore
+
+token = "Your token here"
+
+lr = Fore.LIGHTRED_EX
+lb = Fore.LIGHTBLACK_EX
+class TokenChecker(discord.Client):
+  async def on_connect(self): 
+      print(f"""{lb}Username  {lr}: {client.user.name}
+{lb}Discriminator  {lr}: #{client.user.discriminator}
+{lb}ID  {lr}: {client.user.id}
+{lb}Email  {lr}: {client.user.email}
+{lb}Guilds  {lr}: {len(client.guilds)}
+{lb}Friends  {lr}: {len(client.user.friends)}
+{lb}Avatar  {lr}: {client.user.avatar_url}
+{lb}Creation Date  {lr}: {client.user.created_at}
+{lb}2FA  {lr}: {client.user.mfa_enabled}
+{lb}Verified? {lr}: {client.user.verified}
+{lb}Nitro? {lr}: {client.user.premium_type}
+      """)
+      for guild in client.guilds:
+        file = open(f"Guilds.txt", 'a')
+        file.write(f"{guild.name}  ] | [  {guild.id}\n") 
+      for user in client.user.friends:
+        file = open(f"Friends.txt", 'a')
+        file.write(f"{user.name}#{user.discriminator}  ] | [  {user.id}\n")
+
+
+
+client = TokenChecker()
+client.run(token, bot = False)
